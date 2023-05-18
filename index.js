850d9b28e6164463f87f6c74402284b8a2168d4f
DeepakUser <deepakmegtech@gmail.com>
2023-05-18 14:53:23 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/user');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+
+
+
+
+dotenv.config();
+mongoose
+    .connect(
+        process.env.MONGO_URL
+    )
+    .then(()=>console.log("DB Connection Successfull"))
+    .catch((err)=>{
+        console.log(err);
+    });
+
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
+
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log("Backend server is running");
+})
---
+++ routes/order.js
@@ -0,0 +1,102 @@
+const { json } = require('express');
+const Order = require('../models/Order');
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
+
+const router = require('express').Router();
+
+//Create
+router.post("/", verifyToken, async (req,res)=>{
+
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//UPDATE
+router.put("/:id",verifyTokenAndAdmin, async (req,res)=>{
+       
+    try {
+        const updateOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set:req.body
+            },
+            {
+                new:true
+            }
+        )
+
+        res.status(200).json(updateOrder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+
+// DELETE
+router.delete("/:id",verifyTokenAndAdmin, async(req,res)=>{
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Order has been deleted......")
+    } catch (error) {
+        res.status(500),json(error);
+    }
+});
+
+// GET USER ORDERS
+router.get("/find/:userId", verifyTokenAndAuthorization, async(req,res)=>{
+    try {
+       const orders= await Order.find({userId: req.params.userId});
+       
+       res.status(200).json({orders});
+    } catch (error) {
+        res.status(500),json(error);
+    }
+});
+
+// GET  ALL 
+router.get("/",verifyTokenAndAdmin, async (req,res)=>{
+    try {
+        const orders = await Order.find();
+        res.status(200),json(orders);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//GET MONTHLY INCOME
+
+router.get("/income",verifyTokenAndAdmin,async (req,res)=>{
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth()-1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth()-1));
+
+    try {
+            const income= await Order.aggregate([
+                {$match:{createdAt:{$gte:previousMonth}}},
+                {
+                    $project:{
+                        month:{$month:"$createdAt"},
+                        sales:"$amount",
+                    },  
+                },
+                {
+                    $group:{
+                        _id:"$month",
+                        total:{$sum:"$sales"},
+                    },
+                },
+            ]);
+            res.status(200).json(income);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+})
+
+module.exports=router;
