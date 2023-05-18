3814d17c940d8949609ac172f9f1af4dc78787bb
DeepakUser <deepakmegtech@gmail.com>
2023-05-18 14:57:22 +0530

    Added gitignore file and cros package

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const userRoute = require('./routes/user');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
-
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 
 
@@ -20,13 +21,14 @@ mongoose
     .catch((err)=>{
         console.log(err);
     });
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/products",productRoute);
 app.use("/api/carts",cartRoute);
 app.use("/api/orders",orderRoute);
+app.use("/api/stripes",stripeRoute);
 
 app.listen(process.env.PORT || 8000, ()=>{
     console.log("Backend server is running");
